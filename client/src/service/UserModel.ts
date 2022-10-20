0d67560432be519acfebd07fe703fe349a672a6f
neverswood <nevers_wood@hotmail.fr>
2022-10-20 11:35:25 +0200

    Create the class of user data model

---
+++ client/src/service/UserModel.ts
@@ -0,0 +1,21 @@
+type User = {
+  email: string;
+  firstName: string;
+  lastName: string;
+};
+
+/** Class representing a model for user model */
+export default class UserModel {
+  /**
+   * Create user activiites data model.
+   * @param {User} data - The data value
+   */
+  email;
+  firstName;
+  lastName;
+  constructor(data: User) {
+    this.email = data.email;
+    this.firstName = data.firstName;
+    this.lastName = data.lastName;
+  }
+}
