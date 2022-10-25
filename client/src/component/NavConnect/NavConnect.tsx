58fcd0c226c4133aaf9800df01123cef4eb7c578
neverswood <nevers_wood@hotmail.fr>
2022-10-26 00:06:44 +0200

    Display user name in navbar when logged in

--- client/src/component/NavConnect/NavConnect.tsx
+++ client/src/component/NavConnect/NavConnect.tsx
@@ -1,9 +1,18 @@
+import { useSelector } from 'react-redux';
+
+type State = {
+  authentication: {
+    user: string;
+    token: string;
+  };
+};
 export function NavConnect() {
+  const user = useSelector((state: State) => state.authentication.user);
   return (
     <div>
       <a className="main-nav-item" href="/user">
         <i className="fa fa-user-circle"></i>
-        Tony
+        {user}
       </a>
       <a className="main-nav-item" href="/">
         <i className="fa fa-sign-out"></i>
