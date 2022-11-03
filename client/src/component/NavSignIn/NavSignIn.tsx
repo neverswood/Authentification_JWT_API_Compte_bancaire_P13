3a1ca4a322269b45c05ef319a71aba81d1ba4752
neverswood <nevers_wood@hotmail.fr>
2022-11-03 13:42:59 +0100

    Redirect to signIn page

--- client/src/component/NavSignIn/NavSignIn.tsx
+++ client/src/component/NavSignIn/NavSignIn.tsx
@@ -1,10 +1,21 @@
+import { useNavigate } from 'react-router-dom';
+import { Button } from '../Button/Button';
+
 export function NavSignIn() {
+  const navigate = useNavigate();
+
+  const redirect = () => {
+    navigate('/signIn');
+  };
+
   return (
     <div>
-      <a className="main-nav-item" href="/signIn">
-        <i className="fa fa-user-circle"></i>
-        Sign In
-      </a>
+      <Button classButton="main-nav-item" click={redirect}>
+        <span className="main-nav-item ">
+          <i className="fa fa-user-circle"></i>
+          Sign In
+        </span>
+      </Button>
     </div>
   );
 }
